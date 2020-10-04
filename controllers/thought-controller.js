eb7c67305833a07eb35e7436279591a83263cd8f
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-10-03 21:42:44 -0400

    set up index files for controllers and pseudocoded methods

---
+++ controllers/thought-controller.js
@@ -0,0 +1,19 @@
+const { Thought, Userd } = require('../models');
+
+const thoughtController = {
+  // get all thoughts
+
+  // post new thought (and push to user's thoughts field)
+
+  // get one thought by id
+
+  // update one thought by id
+
+  // delete one thought by id (and pull from user's thoughts field)
+
+  // post new reaction to a thought by id (and push to reactions field)
+
+  // delete a reaction by id from a thought by id (and pull from reactions field)
+};
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,20 @@
+const { User } = require('../models');
+
+const userController = {
+  // get all users
+
+  // post new user
+
+  // get one user by id
+
+  // update one user by id
+
+  // delete one user by id (bonus: delete associated thoughts)
+
+  // post new friend to a user's friend list
+
+  // delete friend from user's friend list
+
+};
+
+module.exports = userController;
